fe1080cd38336601b79e76597f3ff6cef576643d
Sven Speckmaier <sps@ipunkt.biz>
2016-12-06 14:11:52 +0100

    fixed default value syntax

--- root/opt/monit/bin/start-stop.sh
+++ root/opt/monit/bin/start-stop.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 PIDFILE=/opt/varnish/run/varnish.pid
-BIND=${BIND_ADDRESS:0.0.0.0}
-PORT=${PORT:8080}
-CONFIG_FILE=${VARNISH_CONFIG:/opt/varnish/etc/default.vcl}
-SECRET_FILE=${VARNISH_SECRET:/opt/varnish/etc/secret}
+BIND=${BIND_ADDRESS:=0.0.0.0}
+PORT=${PORT:=8080}
+CONFIG_FILE=${VARNISH_CONFIG:=/opt/varnish/etc/default.vcl}
+SECRET_FILE=${VARNISH_SECRET:=/opt/varnish/etc/secret}
 
 case "$1" in
 	start)
-		 echo start-stop-daemon --pidfile "${PIDFIILE}" -S /usr/sbin/varnishd -- \
+		 echo start-stop-daemon --pidfile "${PIDFILE}" -S /usr/sbin/varnishd -- \
 			-a ${BIND}:${PORT} -S "${SECRET_FILE}" -P "${PIDFILE}" \
 			-T "127.0.0.1:8062" -f "${CONFIG_FILE}"
-		 start-stop-daemon --pidfile "${PIDFIILE}" -S /usr/sbin/varnishd -- \
+		 start-stop-daemon --pidfile "${PIDFILE}" -S /usr/sbin/varnishd -- \
 			-a ${BIND}:${PORT} -S "${SECRET_FILE}" -P "${PIDFILE}" \
 			-T "127.0.0.1:8062" -f "${CONFIG_FILE}"
 		;;
 	stop)
-		 start-stop-daemon --pidfile "${PIDFIILE}" -K /usr/sbin/varnishd
+		 start-stop-daemon --pidfile "${PIDFILE}" -K /usr/sbin/varnishd
 		;;
 esac
