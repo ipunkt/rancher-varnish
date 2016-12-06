12b89c69164e66a1604c32f51fe64a6eaf600445
Sven Speckmaier <sps@ipunkt.biz>
2016-12-06 13:48:04 +0100

    added secret and custom varnish_reload_vcl

--- root/opt/monit/bin/start-stop.sh
+++ root/opt/monit/bin/start-stop.sh
@@ -3,11 +3,12 @@ PIDFILE=/opt/varnish/run/varnish.pid
 BIND=${BIND_ADDRESS:0.0.0.0}
 PORT=${PORT:8080}
 CONFIG_FILE=${VARNISH_CONFIG:/opt/varnish/etc/default.vcl}
+SECRET_FILE=${VARNISH_SECRET:/opt/varnish/etc/secret}
 
 case "$1" in
 	start)
 		 start-stop-daemon --pidfile "${PIDFIILE}" -S /usr/sbin/varnishd \
-			-a ${BIND}:${PORT} -P "${PIDFILE}" -f "${CONFIG_FILE}"
+			-a ${BIND}:${PORT} -P "${PIDFILE}" -T "127.0.0.1:8062" -f "${CONFIG_FILE}"
 		;;
 	stop)
 		 start-stop-daemon --pidfile "${PIDFIILE}" -K /usr/sbin/varnishd
