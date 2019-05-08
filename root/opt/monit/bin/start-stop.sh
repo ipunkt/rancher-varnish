eded55aadffffd37087707f9e144084abbce4c9c
svensp <sps@ipunkt.biz>
2019-05-08 10:59:46 +0200

    allow setting storage backend via STORAGE_BACKEND

--- root/opt/monit/bin/start-stop.sh
+++ root/opt/monit/bin/start-stop.sh
@@ -4,15 +4,16 @@ BIND=${BIND_ADDRESS:=0.0.0.0}
 PORT=${PORT:=8080}
 CONFIG_FILE=${VARNISH_CONFIG:=/opt/varnish/etc/default.vcl}
 SECRET_FILE=${VARNISH_SECRET:=/opt/varnish/etc/secret}
+STORAGE_BACKEND=${STORAGE_BACKEND:=malloc,256m}
 
 case "$1" in
 	start)
 		 echo start-stop-daemon --pidfile "${PIDFILE}" -S /usr/sbin/varnishd -- \
-			-a ${BIND}:${PORT} -S "${SECRET_FILE}" -P "${PIDFILE}" \
+			-a ${BIND}:${PORT} -s "${STORAGE_BACKEND}" -S "${SECRET_FILE}" -P "${PIDFILE}" \
 			-T "127.0.0.1:6082" -f "${CONFIG_FILE}"
 		 start-stop-daemon --pidfile "${PIDFILE}" -S /usr/sbin/varnishd -- \
 			-a ${BIND}:${PORT} -S "${SECRET_FILE}" -P "${PIDFILE}" \
-			-T "127.0.0.1:6082" -f "${CONFIG_FILE}"
+			-T "127.0.0.1:6082" -s "${STORAGE_BACKEND}" -f "${CONFIG_FILE}"
 		;;
 	stop)
 		 start-stop-daemon --pidfile "${PIDFILE}" -K /usr/sbin/varnishd
